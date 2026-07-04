7fed5fd42d56f5cbc1cd8bfe9df9f694e38cf3a9
Anamika <anamikasharma16649@gmail.com>
2026-07-04 22:19:03 +0530

    Crate a Post API Endpoint for the news classification system in FastAPI.

--- main.py
+++ main.py
@@ -22,24 +22,24 @@ class NewsReqst(BaseModel):
 async def root():
     return {"message": "Welcome to the News Classification System"}
 
-# @app.post("/classify")
-# async def classify_news(news_request: NewsReqst):
+@app.post("/classify")
+async def classify_news(news_request: NewsReqst):
 
-#     if not news_request.news.strip():
-#         raise HTTPException(
-#             status_code=400, detail="News article cannot be empty."
-#         )
+    if not news_request.news.strip():
+        raise HTTPException(
+            status_code=400, detail="News article cannot be empty."
+        )
 
-#     try:
-#         prediction = int(model.predict([news_request.news])[0])
-#         return {
-#             "success": True,
-#             "class_id": prediction,
-#             "category": CATEGORY_MAP[prediction]["name"],
-#             "icon": CATEGORY_MAP[prediction]["icon"],
-#         }
+    try:
+        prediction = int(model.predict([news_request.news])[0])
+        return {
+            "success": True,
+            "class_id": prediction,
+            "category": CATEGORY_MAP[prediction]["name"],
+            "icon": CATEGORY_MAP[prediction]["icon"],
+        }
 
-#     except Exception as e:
-#         raise HTTPException(
-#             status_code=500, detail= str(e)
-#         )
+    except Exception as e:
+        raise HTTPException(
+            status_code=500, detail= str(e)
+        )
